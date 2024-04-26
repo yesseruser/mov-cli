32f263857a84c158fd28e8dca5abf56ca5f92891
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-04-26 13:49:32 +0100

    fix: platform default scrapers should not display in ``mov-cli -lp`` and added a plugin class

--- mov_cli/cli/plugins.py
+++ mov_cli/cli/plugins.py
@@ -4,15 +4,16 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import Tuple, List, Dict, NoReturn
 
-    from ..plugins import PluginHookData
+    from ..plugins import Plugin
+    PluginsDataT = List[Tuple[str, str, Plugin]]
 
 from devgoldyutils import Colours
 
 from ..plugins import load_plugin
 from ..logger import mov_cli_logger
 
-def get_plugins_data(plugins: Dict[str, str]) -> List[Tuple[str, str, PluginHookData]]:
-    plugins_data: List[Tuple[str, str, PluginHookData]] = []
+def get_plugins_data(plugins: Dict[str, str]) -> PluginsDataT:
+    plugins_data: PluginsDataT = []
 
     for plugin_namespace, plugin_module_name in plugins.items():
         plugin = load_plugin(plugin_module_name)
@@ -20,33 +21,23 @@ def get_plugins_data(plugins: Dict[str, str]) -> List[Tuple[str, str, PluginHook
         if plugin is None:
             continue
 
-        plugin_data, _ = plugin
-
-        if plugin_data is None:
-            continue
-
         plugins_data.append(
-            (plugin_namespace, plugin_module_name, plugin_data, plugin)
+            (plugin_namespace, plugin_module_name, plugin)
         )
 
     return plugins_data
 
 def show_all_plugins(plugins: Dict[str, str]) -> None:
 
-    for plugin_namespace, plugin_module_name, plugin_hook_data, plugin in get_plugins_data(plugins):
+    for plugin_namespace, plugin_module_name, plugin in get_plugins_data(plugins):
 
         if plugin is not None:
-            plugin_module = plugin[1]
-
-            plugin_version = getattr(plugin_module, "__version__", "N/A")
+            plugin_version = getattr(plugin.module, "__version__", "N/A")
 
             print(f"- {Colours.PURPLE.apply(plugin_module_name)} ({plugin_namespace}) [{Colours.BLUE.apply(plugin_version)}]")
 
-            for scraper_name in plugin_hook_data["scrapers"]:
-                if scraper_name == "DEFAULT":
-                    continue
-
-                print(f"  - {Colours.PINK_GREY.apply(scraper_name)}")
+            for scraper_name in plugin.scrapers:
+                print(f"  - {Colours.PINK_GREY.apply(scraper_name[0])}")
 
 def handle_internal_plugin_error(e: Exception) -> NoReturn:
     mov_cli_logger.critical(
--- mov_cli/plugins.py
+++ mov_cli/plugins.py
@@ -6,18 +6,20 @@ from typing import TYPE_CHECKING, TypedDict
 
 if TYPE_CHECKING:
     from types import ModuleType
-    from typing import Optional, Dict, Literal, Tuple
+    from typing import Optional, Dict, Literal, List, Tuple
 
     from .scraper import Scraper
 
 import importlib
+from dataclasses import dataclass
 from devgoldyutils import LoggerAdapter
 
 from .logger import mov_cli_logger
 
 __all__ = (
     "load_plugin", 
-    "PluginHookData"
+    "PluginHookData", 
+    "Plugin"
 )
 
 logger = LoggerAdapter(mov_cli_logger, prefix = "Plugins")
@@ -29,16 +31,38 @@ class PluginHookData(TypedDict):
     """The name of the pypi package. This is required for the plugin update notifier to work."""
     scrapers: Dict[str, Scraper] | Dict[Literal["DEFAULT"], Scraper]
 
-def load_plugin(module_name: str) -> Optional[Tuple[Optional[PluginHookData], ModuleType]]:
+@dataclass
+class Plugin:
+    module: ModuleType
+    hook_data: PluginHookData
+
+    @property
+    def scrapers(self) -> List[Tuple[str, Scraper]]:
+        non_default_scrapers = []
+
+        for scraper_namespace, scraper_class in self.hook_data["scrapers"].items():
+
+            if scraper_namespace.endswith("DEFAULT"):
+                continue
+
+            non_default_scrapers.append((scraper_namespace, scraper_class))
+
+        return non_default_scrapers
+
+def load_plugin(module_name: str) -> Optional[Plugin]:
     try:
         plugin_module = importlib.import_module(module_name.replace("-", "_"))
     except ModuleNotFoundError as e:
         logger.error(f"Failed to import a plugin from the module '{module_name}'! Error --> {e}")
         return None
 
-    plugin_data = getattr(plugin_module, "plugin", None)
+    plugin_data: PluginHookData = getattr(plugin_module, "plugin", None)
 
     if plugin_data is None:
         logger.warning(f"Failed to load the plugin '{module_name}'! It doesn't contain a plugin hook!")
+        return None
 
-    return plugin_data, plugin_module
+    return Plugin(
+        module = plugin_module, 
+        hook_data = plugin_data
+    )

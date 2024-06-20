f0afe77a8c723cf2a5df3df79c7bb03002c32d93
Ananas <ananas@ananas.moe>
2024-06-20 15:48:08 +0200

    fix(hide_ip): check if it's a string

--- mov_cli/utils/ip.py
+++ mov_cli/utils/ip.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
 import re
 
 __all__ = ("hide_ip",)
 
-def hide_ip(text: str, hide_it: bool) -> str:
+def hide_ip(text: str, hide_it: bool) -> str | Any:
     ipv4_re = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
     ipv6_re = "([a-f0-9:]+:+)+[a-f0-9]+"
 
-    if hide_it:
+    if hide_it and isinstance(text, str):
         text = re.sub(ipv4_re, "{the-cat-snatched-your-ip-address}", text)
         text = re.sub(ipv6_re, "{the-cat-snatched-your-ip-address}", text)
 
